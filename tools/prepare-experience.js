7a8e20c14bcd2ee09b79448002e8da8d291cc554
kirillston <stonozhenko@anytype.io>
2024-02-27 16:33:28 +0300

    Run prepare experience before validation

--- tools/prepare-experience.js
+++ tools/prepare-experience.js
@@ -1,27 +1,25 @@
 const fs = require('fs');
 const issue = require('../issue.json');
-const [issueExperience, issueTitle, issueScreenshot, issueDescription, issueCategories, issueLanguage, trash, issueAuthor, name, id] = Object.values(issue);
 
-const author = "https://github.com/" + issueAuthor;
+const name = issue.name
+const author = "https://github.com/" + issue.author;
 const license = "MIT";
-const categories = issueCategories.split(", ");
+const categories = issue.categories.split(", ");
 
 var experience = {
   $schema: "https://tools.gallery.any.coop/experience.schema.json",
-  id: id,
+  id: issue.id,
   name: name,
   author: author,
   license: license,
-  title: issueTitle,
-  description: issueDescription,
+  title: issue.title,
+  description: issue.description,
   categories: categories,
-  language: issueLanguage,
+  language: issue.language,
   screenshots: [],
   downloadLink: "",
   fileSize: 0
 }
-
-console.log(`Issie.json info: exp=${issueExperience}, title=${issueTitle}, scr=${issueScreenshot}, desc=${issueDescription}, cat=${issueCategories}, lang=${issueLanguage}, auth=${issueAuthor}, name=${name}, id=${id}`)
 
 if (!fs.existsSync('experiences')) {
   fs.mkdirSync('experiences');
@@ -35,10 +33,10 @@ if (fs.existsSync(dir)) {
 }
 
 var regex = /https:\/\/github.com\/.*\/.*\/.*\/.*\.zip/g;
-const zipLink = issueExperience.match(regex)[0];
+const zipLink = issue.experience.match(regex)[0];
 
 if (!zipLink) {
-  throw new Error(`Experience with name ${name} has no link to zip archive`);
+  throw new Error(`Experience has no link to zip archive`);
 }
 
 (async () => {
@@ -47,45 +45,47 @@ if (!zipLink) {
   fs.writeFileSync(`${dir}/${name}.zip`, Buffer.from(buffer));
 })();
 
-// const testDownloadLink = "https://github.com/anyproto/gallery/raw/" + name + "/experiences/" + name + "/" + name + ".zip"
-// const prodDownloadLink = "https://storage.gallery.any.coop/" + name + "/" + name + ".zip"
-//
-// var regex = /src\s*=\s*"(.+?)"/g;
-// const extension = "png";
-// const screenshotLinks = issueScreenshot.match(regex);
-// const screenshotsDir = `${dir}/screenshots`;
-//
-// if (!screenshotLinks) {
-//   throw new Error(`Experience with name ${name} has no screenshots`);
-// }
-//
-// var testScreenshotLinks = [];
-// var prodScreenshotLinks = [];
-// for (var i = 1; i <= screenshotLinks.length; i++) {
-//   testScreenshotLinks.push("https://github.com/anyproto/gallery/raw/" + name + "/experiences/" + name + "/screenshots/screenshot-" + i + "." + extension);
-//   prodScreenshotLinks.push("https://storage.gallery.any.coop/" + name + "/screenshots/screenshot-" + i + "." + extension);
-// }
-//
-//
-// if (fs.existsSync(screenshotsDir)) {
-//   console.log(`Screenshots for experience with name ${name} already exists`);
-// } else {
-//   fs.mkdirSync(screenshotsDir);
-// }
-//
-// var count = 1;
-// screenshotLinks.forEach(async (link) => {
-//   const response = await fetch(link.replace(/src="/, '').replace(/"/, ''));
-//   const buffer = await response.arrayBuffer();
-//   fs.writeFileSync(`${screenshotsDir}/screenshot-${count}.${extension}`, Buffer.from(buffer));
-//   count++;
-// });
-//
-//
-// experience.downloadLink = testDownloadLink
-// experience.screenshots = testScreenshotLinks
-// fs.writeFileSync(`${dir}/test-manifest.json`, JSON.stringify(experience, null, 2));
-//
-// experience.downloadLink = prodDownloadLink
-// experience.screenshots = prodScreenshotLinks
-// fs.writeFileSync(`${dir}/manifest.json`, JSON.stringify(experience, null, 2));
+const testDownloadLink = "https://github.com/anyproto/gallery/raw/" + name + "/experiences/" + name + "/" + name + ".zip"
+const prodDownloadLink = "https://storage.gallery.any.coop/" + name + "/" + name + ".zip"
+
+var regex = /src\s*=\s*"(.+?)"/g;
+const extension = "png";
+var screenshotLinks = issue.screenshots.match(regex);
+const screenshotsDir = `${dir}/screenshots`;
+
+if (!screenshotLinks) {
+  var regex = /\((.*?)\)/g;
+  screenshotLinks = issue.screenshots.match(regex);
+  screenshotLinks = screenshotLinks.map(link => link.replace(/\(|\)/g, ''));
+}
+
+var testScreenshotLinks = [];
+var prodScreenshotLinks = [];
+for (var i = 1; i <= screenshotLinks.length; i++) {
+  testScreenshotLinks.push("https://github.com/anyproto/gallery/raw/" + name + "/experiences/" + name + "/screenshots/screenshot-" + i + "." + extension);
+  prodScreenshotLinks.push("https://storage.gallery.any.coop/" + name + "/screenshots/screenshot-" + i + "." + extension);
+}
+
+
+if (fs.existsSync(screenshotsDir)) {
+  console.log(`Screenshots for experience with name ${name} already exists`);
+} else {
+  fs.mkdirSync(screenshotsDir);
+}
+
+var count = 1;
+screenshotLinks.forEach(async (link) => {
+  const response = await fetch(link.replace(/src="/, '').replace(/"/, ''));
+  const buffer = await response.arrayBuffer();
+  fs.writeFileSync(`${screenshotsDir}/screenshot-${count}.${extension}`, Buffer.from(buffer));
+  count++;
+});
+
+
+experience.downloadLink = testDownloadLink
+experience.screenshots = testScreenshotLinks
+fs.writeFileSync(`${dir}/test-manifest.json`, JSON.stringify(experience, null, 2));
+
+experience.downloadLink = prodDownloadLink
+experience.screenshots = prodScreenshotLinks
+fs.writeFileSync(`${dir}/manifest.json`, JSON.stringify(experience, null, 2));
